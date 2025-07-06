9a989948379a2cf3b068e363860eae27dd617191
DaryPet <d7akkord@gmail.com>
2025-07-06 15:40:54 +0200

    queries

--- hw3_queries.sql
+++ hw3_queries.sql
@@ -1,8 +1,25 @@
 USE goit_rdb_hw_03;
 
+SELECT * FROM products;
+SELECT name, phone FROM shippers;
+
+SELECT 
+    AVG(price) AS average_price,
+    MAX(price) AS max_price,
+    MIN(price) AS min_price
+FROM products;
+
+SELECT DISTINCT category_id, price
+FROM products
+ORDER BY price DESC
+LIMIT 10;
+
+SELECT COUNT(*) AS products_count
+FROM products
+WHERE price BETWEEN 20 AND 100;
+
 SELECT supplier_id, 
        COUNT(*) AS products_count,
        AVG(price) AS average_price
 FROM products
 GROUP BY supplier_id;
-
